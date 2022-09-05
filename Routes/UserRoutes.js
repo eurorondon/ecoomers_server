606bb190573b80e1cf963a6bf42abd29df3848ca
eurorondon03 <eurorondon03@gmail.com>
2022-09-05 02:21:01 -0400

    finalizado capitulo llamado -> REGISTER

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -8,7 +8,7 @@ const userRouter = express.Router();
 
 
 
-//LOGIN USSER
+//LOGIN 
 userRouter.post("/login", asyncHandler(
     async(req, res) => {
         const {email, password} = req.body
@@ -28,6 +28,39 @@ userRouter.post("/login", asyncHandler(
             throw new Error("Invalid Email or Password")
         }
     })
+);
+
+
+// REGISTER
+userRouter.post("/",  asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      res.status(400);
+      throw new Error("User already exists");
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400);
+      throw new Error("Invalid User Data");
+    }
+  })
 );
 
 
